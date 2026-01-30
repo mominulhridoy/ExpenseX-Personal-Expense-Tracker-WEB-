5d435fb9d87b84e6e978991b0ed24889fdcc42ca
Mominul Islam Hridoy <107831002+mominulhridoy@users.noreply.github.com>
2026-01-30 22:10:21 +0600

    Update connection.php

--- Database_Setup/connection.php
+++ Database_Setup/connection.php
@@ -1,4 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "expense_tracker");
-if ($conn->connect_error) { die("Database connection failed"); }
+$host = "localhost";
+$user = "root"; 
+$pass = ""; 
+$dbname = "expense_tracker";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
