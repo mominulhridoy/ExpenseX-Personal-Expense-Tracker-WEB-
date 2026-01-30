131ba1940fa3ac01252f9084836d1be2e5750c46
Mominul Islam Hridoy <107831002+mominulhridoy@users.noreply.github.com>
2026-01-30 22:02:59 +0600

    Remove database connection configuration

--- config/db.php
+++ config/db.php
@@ -1,14 +1,1 @@
-<?php
-$host = "localhost";
-$user = "root"; 
-$pass = ""; 
-$dbname = "expense_tracker";
 
-// Connect to MySQL
-$conn = new mysqli($host, $user, $pass, $dbname);
-
-// Check if it worked
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
