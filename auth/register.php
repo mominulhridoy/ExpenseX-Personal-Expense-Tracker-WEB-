01dd8d43bb3d469786c39f42db1476fcdb715b5a
Mominul Islam Hridoy <107831002+mominulhridoy@users.noreply.github.com>
2026-03-18 23:35:13 +0600

    Update register.php

--- auth/register.php
+++ auth/register.php
@@ -1,13 +1,1 @@
-<?php
-// Placeholder for register.php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Register - Expense Tracker</title>
-</head>
-<body>
-    <h1>Register Page</h1>
-</body>
-</html>
+
